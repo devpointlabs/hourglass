7bb50ce2bfe85b56f8441c86671f9566932bf268
Joshua Larsen <larsen.bass@gmail.com>
2019-04-01 15:47:40 -0600

    initial commit add user auth

---
+++ client/src/components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Header, } from 'semantic-ui-react';
+
+const Home = () => (
+  <Header as="h3" textAlign="center">Hour Glass, Get er Done</Header>
+)
+
+export default Home;
