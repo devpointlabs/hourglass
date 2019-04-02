ff83ed7471444cab7c2680772f4010459a17dda6
Emma Scanlon <47067945+emmascanlon@users.noreply.github.com>
2019-04-02 14:35:49 -0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  has_many :projects, through: :assignments
+  has_many :projects, through: :assignments dependent: :destroy
   
 end
