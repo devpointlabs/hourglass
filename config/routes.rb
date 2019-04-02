1b14026356131ced541e7130da72a3b3acb78b62
Kaleb Allen <7kaleb29allen@gmail.com>
2019-04-02 14:32:51 -0600

    routes for timeboards

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
 
   namespace :api do
-    resources :projects, only: [:index, :update]
+    resources :projects, only: [:index, :update] do
+      resources :timeboards
+    end
   end
 end
