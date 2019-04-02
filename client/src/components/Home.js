3a06af8c4eb9093cb0c1ec14550076da81db6c3c
Joshua Larsen <larsen.bass@gmail.com>
2019-04-02 11:14:22 -0600

    fixed math issue, current itteration stores object in state

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Header, } from 'semantic-ui-react';
+import StopWatch from './StopWatch'
 
 const Home = () => (
+  <>
   <Header as="h3" textAlign="center">Hour Glass, Get er Done</Header>
+  <StopWatch />
+  </>
 )
 
 export default Home;
