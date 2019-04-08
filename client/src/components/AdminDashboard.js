666789da0124480b1cc807cd3415385b6a08c81f
Niko Dyer <nikodyer44@gmail.com>
2019-04-08 15:36:55 -0600

    edited admin dashboard

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -2,44 +2,58 @@ import React from "react";
 import axios from "axios";
 import { Table, Header, Image, Input } from "semantic-ui-react";
 
+const defaultImage = "https://d30y9cdsu7xlg0.cloudfront.net/png/15724-200.png";
+
 class hoursTable extends React.Component {
-  state = { users: [] };
+  state = { users: [], projects: [] };
 
   componentDidMount() {
     axios
       .get("/api/users")
       .then(response => this.setState({ users: response.data }));
+    axios
+      .get("/api/projects")
+      .then(response => this.setState({ projects: response.data }));
   }
 
   render() {
     return (
-      <Table basic="very" celled collapsing>
+      <Table definition size="small" striped>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>Employee</Table.HeaderCell>
-            <Table.HeaderCell>Total Hours</Table.HeaderCell>
-            <Table.HeaderCell>Hourly Wage</Table.HeaderCell>
+            <Table.HeaderCell />
+            {this.state.projects.map(project => (
+              <Table.HeaderCell>{project.name}</Table.HeaderCell>
+            ))}
+            <Table.HeaderCell>Total</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
         {this.state.users.map(user => (
           <Table.Body>
             <Table.Row>
-              <Table.Cell>
-                <Header as="h4" image>
-                  <Image src={user.image} rounded size="massive" />
-                  <Header.Content>
-                    {user.name}
-                    <Header.Subheader>{user.nickname}</Header.Subheader>
-                  </Header.Content>
+              <Table.Cell size="small">
+                <Header as="h4">
+                  <Image circular src={user.image || defaultImage} />
+                  {user.name}
                 </Header>
               </Table.Cell>
-              <Table.Cell>
-                <Input focus placeholder="Hours" type="number" />
-              </Table.Cell>
-              <Table.Cell>$9.00</Table.Cell>
+              {this.state.projects.map(project => (
+                <Table.Cell>
+                  {project.planned_start} - {project.planned_end}
+                </Table.Cell>
+              ))}
+              <Table.Cell>Total</Table.Cell>
             </Table.Row>
           </Table.Body>
         ))}
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>Total</Table.Cell>
+            {this.state.projects.map(project => (
+              <Table.Cell>total</Table.Cell>
+            ))}
+          </Table.Row>
+        </Table.Body>
       </Table>
     );
   }
