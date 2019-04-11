3b47c1b7fafae8e985b36eb28377488a301e6c9a
Joshua Larsen <larsen.bass@gmail.com>
2019-04-11 10:22:05 -0600

    added weekview clickable and changed color on login and register to match rebecca purple

--- client/src/components/timeblocks/WeekViewTableHeaderRow.js
+++ client/src/components/timeblocks/WeekViewTableHeaderRow.js
@@ -5,13 +5,21 @@ import moment from "moment";
 
 class WeekViewTableHeaderRow extends React.Component {
   render() {
+    const {
+      monday,
+      selectedDate,
+      setSelectedDate,
+      setSelectedWeek
+    } = this.props;
     return (
       <Table.Row style={{ background: "#e2e2e2" }}>
         <Table.HeaderCell style={{ paddingTop: "10px", paddingLeft: "1%" }} />
         <Table.HeaderCell style={{ width: "20%" }} />
         <WeekViewTableHeaderLabels
-          selectedDate={this.props.selectedDate}
-          monday={this.props.monday}
+          selectedDate={selectedDate}
+          monday={monday}
+          setSelectedDate={setSelectedDate}
+          setSelectedWeek={setSelectedWeek}
         />
         <Table.HeaderCell />
         <Table.HeaderCell />
