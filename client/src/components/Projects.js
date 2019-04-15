785defe064e6e015421b41148ee2d0769dad9d24
Kaleb Allen <7kaleb29allen@gmail.com>
2019-04-15 11:05:01 -0600

    project view with budget now

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -2,7 +2,15 @@ import React from "react";
 import axios from "axios";
 import ProjectForm from "./ProjectForm";
 import { Link } from "react-router-dom";
-import { Grid, Button, Container, Icon } from "semantic-ui-react";
+import {
+  Grid,
+  Button,
+  Container,
+  Icon,
+  Table,
+  Progress
+} from "semantic-ui-react";
+import BudgetView from "./BudgetView";
 
 class Projects extends React.Component {
   state = { projects: [], editing: false };
@@ -15,19 +23,23 @@ class Projects extends React.Component {
 
   showProjects = () => {
     return this.state.projects.map(p => (
-      <tbody>
-        <tr>
-          <td>
-            <Link
-              style={{ color: "RebeccaPurple", fontWeight: "bold" }}
-              to={`/projects/${p.id}`}
-            >
-              {p.name ? p.name : "Project has no name"}
-            </Link>
-          </td>
-          <td>{p.client_name}</td>
-        </tr>
-      </tbody>
+      <Table.Row key={p.id}>
+        <Table.Cell>
+          <Link
+            style={{ color: "RebeccaPurple", fontWeight: "bold" }}
+            to={`/projects/${p.id}`}
+          >
+            {p.name ? p.name : "Project has no name"}
+          </Link>
+        </Table.Cell>
+        <Table.Cell>{p.client_name}</Table.Cell>
+        <Table.Cell>{p.budget}</Table.Cell>
+        <Table.Cell>$$$$$$</Table.Cell>
+        <Table.Cell>
+          <Progress color="violet" percent="80" />
+        </Table.Cell>
+        <Table.Cell>$$$$</Table.Cell>
+      </Table.Row>
     ));
   };
 
@@ -64,6 +76,10 @@ class Projects extends React.Component {
                 <tr>
                   <th>Project</th>
                   <th>Client</th>
+                  <th>Budget</th>
+                  <th>Spent</th>
+                  <th />
+                  <th>Remaining</th>
                 </tr>
               </thead>
               {this.showProjects()}
