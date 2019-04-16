4b9d4224457fedc609932e707cf8cb154131614f
Joshua Larsen <larsen.bass@gmail.com>
2019-04-16 17:27:31 -0600

    merge

--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -13,34 +13,54 @@ import {
 } from "semantic-ui-react";
 import BudgetView from "./BudgetView";
 
+
 class Projects extends React.Component {
-  state = { projects: [], editing: false };
+  state = { projects: [], toggleForm: false, project: {} };
 
   componentDidMount() {
     axios
       .get("api/project/projects_with_data")
       .then(res => this.setState({ projects: res.data }));
   }
+  handleToggle = (project) => {
+    this.setState({ toggleForm: !this.state.toggleForm, project: project });
+  };
 
   showProjects = () => {
     return this.state.projects.map(p => (
-      <Table.Row key={p.project_id}>
-        <Table.Cell>
-          <Link
-            style={{ color: "RebeccaPurple", fontWeight: "bold" }}
-            to={`/projects/${p.project_id}`}
-          >
-            {p.project_name ? p.project_name : "Project has no name"}
-          </Link>
-        </Table.Cell>
-        <Table.Cell>{p.client_name}</Table.Cell>
-        <Table.Cell>{p.budget}</Table.Cell>
-        <Table.Cell>{p.total_project_cost}</Table.Cell>
-        <Table.Cell>
-          <Progress color="violet" percent={p.percent_spent} />
-        </Table.Cell>
-        <Table.Cell>{p.percent_spent}</Table.Cell>
-      </Table.Row>
+      <>
+        <Table.Row key={p.project_id}>
+          <Table.Cell>
+            <Link
+              style={{ color: "RebeccaPurple", fontWeight: "bold" }}
+              to={`/projects/${p.project_id}`}
+            >
+              {p.project_name ? p.project_name : "Project has no name"}
+            </Link>
+          </Table.Cell>
+          <Table.Cell>{p.client_name}</Table.Cell>
+          <Table.Cell>{p.budget}</Table.Cell>
+          <Table.Cell>{p.total_project_cost}</Table.Cell>
+          <Table.Cell style={{ paddingTop: '35px', }}>
+            <Progress color="violet" percent={p.percent_spent} />
+          </Table.Cell>
+          <Table.Cell>{p.percent_spent}</Table.Cell>
+          <Table.Cell>
+            <Icon name='cog' style={{ color: "RebeccaPurple" }} onClick={() => this.handleToggle(p)} />
+          </Table.Cell>
+
+        </Table.Row>
+        <div>
+          {this.state.toggleForm ? (
+            <ProjectForm
+              project={this.state.project}
+              updateSubmit={this.updateSubmit}
+            />
+          ) : (
+              <div />
+            )}
+        </div>
+      </>
     ));
   };
 
@@ -52,6 +72,7 @@ class Projects extends React.Component {
             <Menu.Item>Add New Project</Menu.Item>
           </Link>
         </Menu>
+
         <Container>
           {this.state.editing ? (
             <ProjectForm
@@ -74,13 +95,17 @@ class Projects extends React.Component {
                     <th>Spent</th>
                     <th />
                     <th>Perecent Spent</th>
+                    <th style={{ width: '5%' }}>Edit</th>
                   </tr>
                 </thead>
                 {this.showProjects()}
+
               </table>
+
             </Grid.Column>
             {/* </Grid.Row> */}
           </Grid>
+
         </Container>
       </>
     );
