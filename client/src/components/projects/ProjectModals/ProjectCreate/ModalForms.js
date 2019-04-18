7e85dcc938f82c5d1ff6a44bfd5dd90f1567d642
Joshua Larsen <larsen.bass@gmail.com>
2019-04-18 14:42:08 -0600

    modal-fix

--- client/src/components/projects/ProjectModals/ProjectCreate/ModalForms.js
+++ client/src/components/projects/ProjectModals/ProjectCreate/ModalForms.js
@@ -5,14 +5,17 @@ import {
 } from "semantic-ui-react";
 import NewProject from './NewProject'
 import NewTaskTable from './NewTaskTable'
+import EditTeamModalv2 from '../../EditTeamModalv2'
 
 
 class ModalForms extends React.Component {
   state = {
     modalOpen: false,
     modalOpen2: false,
-    modalOpen3: false
+    modalOpen3: false,
+    project_id: 'word'
   }
+
 
   handleClose = () => {
     this.setState({ modalOpen: false })
@@ -37,6 +40,10 @@ class ModalForms extends React.Component {
     this.setState({ modalOpen3: false })
   }
 
+  setProjectId = (project_id) => {
+    this.setState({ project_id: project_id })
+  }
+
   render() {
 
     return (
@@ -47,24 +54,66 @@ class ModalForms extends React.Component {
         <Modal
           open={this.state.modalOpen}
           onClose={() => this.handleClose()}
-          centered={false} style={{ width: '700px' }}>
+          style={{ width: '700px' }}>
           <Modal.Header style={{ background: 'RebeccaPurple', color: 'white' }}>New Project</Modal.Header>
           <Modal.Content>
             <NewProject
               openModal2={this.openModal2}
               handleClose={this.handleClose}
+              setProjectId={this.setProjectId}
             />
           </Modal.Content>
         </Modal>
         <Modal
           open={this.state.modalOpen2}
           onClose={() => this.closeModal2()}
-          centered={false} style={{ width: '850px' }}>
+          style={{ width: '850px' }}>
           <Modal.Header style={{ background: 'RebeccaPurple', color: 'white' }}>Add Tasks</Modal.Header>
           <Modal.Content>
             <NewTaskTable
               closeModal2={this.closeModal2}
               openModal3={this.openModal3}
+              setProjectId={this.setProjectId}
+            />
+          </Modal.Content>
+        </Modal>
+        <Modal open={this.state.modalOpen3} onClose={() => this.closeModal3()}>
+          <Modal.Header style={{ background: "rebeccapurple", color: "white" }}>
+            <div>Add Team Members</div>
+          </Modal.Header>
+          <div>
+            <div style={{ display: "flex" }}>
+              <div
+                style={{
+                  width: "50%",
+                  textAlign: "left",
+                  padding: "10px",
+                  paddingLeft: "40px",
+                  fontWeight: "bold"
+                }}
+              >
+                Users
+              </div>
+              <div
+                style={{
+                  fontWeight: "bold",
+                  width: "50%",
+                  textAlign: "left",
+                  padding: "10px"
+                }}
+              >
+                Assigned To Project
+              </div>
+            </div>
+          </div>
+          <Modal.Content>
+            <EditTeamModalv2
+              users={this.state.users}
+              project_id={this.state.project_id}
+              handleClose={this.closeModal3}
+              getUsersAndHours={this.closeModal3}
+              users={[]}
+              newProjectType={'newProjectType'}
             />
           </Modal.Content>
         </Modal>
