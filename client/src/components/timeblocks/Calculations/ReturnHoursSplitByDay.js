ac4ba8be06cc40a010a140daa3aa869bb54cb75c
Chris Larsen <c.r.larsen@hotmail.com>
2019-04-20 17:32:07 -0600

    row add button

--- client/src/components/timeblocks/Calculations/ReturnHoursSplitByDay.js
+++ client/src/components/timeblocks/Calculations/ReturnHoursSplitByDay.js
@@ -2,49 +2,44 @@ import moment from "moment";
 
 export const returnHoursSplitByDay = (week, monday) => {
   let mondayTimeBlocks = week.filter(
-    wtb => moment(wtb.start_time).format("dd") === moment(monday).format("dd")
+    wtb => moment(wtb.start_time).format("dd") === "Mo"
+    //moment(monday).format("dd")
   );
   let tuesdayTimeBlocks = week.filter(
-    wtb =>
-      moment(wtb.start_time).format("dd") ===
-      moment(monday)
-        .add(1, "days")
-        .format("dd")
+    wtb => moment(wtb.start_time).format("dd") === "Tu"
+    // moment(monday)
+    //   .add(1, "days")
+    //   .format("dd")
   );
   let wednesdayTimeBlocks = week.filter(
-    wtb =>
-      moment(wtb.start_time).format("dd") ===
-      moment(monday)
-        .add(2, "days")
-        .format("dd")
+    wtb => moment(wtb.start_time).format("dd") === "We"
+    // moment(monday)
+    //   .add(2, "days")
+    //   .format("dd")
   );
   let thursdayTimeBlocks = week.filter(
-    wtb =>
-      moment(wtb.start_time).format("dd") ===
-      moment(monday)
-        .add(3, "days")
-        .format("dd")
+    wtb => moment(wtb.start_time).format("dd") === "Th"
+    // moment(monday)
+    //   .add(3, "days")
+    //   .format("dd")
   );
   let fridayTimeBlocks = week.filter(
-    wtb =>
-      moment(wtb.start_time).format("dd") ===
-      moment(monday)
-        .add(4, "days")
-        .format("dd")
+    wtb => moment(wtb.start_time).format("dd") === "Fr"
+    // moment(monday)
+    //   .add(4, "days")
+    //   .format("dd")
   );
   let saturdayTimeBlocks = week.filter(
-    wtb =>
-      moment(wtb.start_time).format("dd") ===
-      moment(monday)
-        .add(5, "days")
-        .format("dd")
+    wtb => moment(wtb.start_time).format("dd") === "Sa"
+    // moment(monday)
+    //   .add(5, "days")
+    //   .format("dd")
   );
   let sundayTimeBlocks = week.filter(
-    wtb =>
-      moment(wtb.start_time).format("dd") ===
-      moment(monday)
-        .add(6, "days")
-        .format("dd")
+    wtb => moment(wtb.start_time).format("dd") === "Su"
+    // moment(monday)
+    //   .add(6, "days")
+    //   .format("dd")
   );
   let mondayHours = mondayTimeBlocks.reduce(
     (sum, block) => sum + parseFloat(block.hours),
--- client/src/components/timeblocks/WeekView/NewRowForm.js
+++ client/src/components/timeblocks/WeekView/NewRowForm.js
@@ -17,8 +17,24 @@ class NewRowForm extends React.Component {
     sundayTotal: "",
     weekTotal: "",
     project: "",
-    task: {}
+    task: {},
+    mondaySubmitComplete: true,
+    tuesdaySubmitComplete: true,
+    wednesdaySubmitComplete: true,
+    thursdaySubmitComplete: true,
+    fridaySubmitComplete: true,
+    saturdaySubmitComplete: true,
+    sundaySubmitComplete: true
   };
+
+  componentDidMount() {
+    const t = this.props.tasks[0] && this.props.tasks[0];
+    const p = t.projectInfo;
+    this.setState({
+      project: { value: p.id, label: `${p.name} (${p.client_name})` },
+      task: { value: t.id, label: t.name }
+    });
+  }
 
   handleChange = e => {
     this.setState(
@@ -54,44 +70,69 @@ class NewRowForm extends React.Component {
   };
 
   handleSubmit = e => {
-    const { monday } = this.props;
-    const mondayStartOfDay = moment(monday).startOf("day");
-    this.state.mondayTotal &&
-      this.submitBlock(this.state.mondayTotal, mondayStartOfDay);
-    this.state.tuesdayTotal &&
-      this.submitBlock(
-        this.state.tuesdayTotal,
-        moment(mondayStartOfDay).add(1, "days")
-      );
-    this.state.wednesdayTotal &&
-      this.submitBlock(
-        this.state.wednesdayTotal,
-        moment(mondayStartOfDay).add(2, "days")
-      );
-    this.state.thursdayTotal &&
-      this.submitBlock(
-        this.state.thursdayTotal,
-        moment(mondayStartOfDay).add(3, "days")
-      );
-    this.state.fridayTotal &&
-      this.submitBlock(
-        this.state.fridayTotal,
-        moment(mondayStartOfDay).add(4, "days")
-      );
-    this.state.saturdayTotal &&
-      this.submitBlock(
-        this.state.saturdayTotal,
-        moment(mondayStartOfDay).add(5, "days")
-      );
-    this.state.sundayTotal &&
-      this.submitBlock(
-        this.state.sundayTotal,
-        moment(mondayStartOfDay).add(6, "days")
-      );
-    this.props.toggleShowNewRow();
+    this.setState(
+      {
+        mondaySubmitComplete: false,
+        tuesdaySubmitComplete: false,
+        wednesdaySubmitComplete: false,
+        thursdaySubmitComplete: false,
+        fridaySubmitComplete: false,
+        saturdaySubmitComplete: false,
+        sundaySubmitComplete: false
+      },
+      () => {
+        const { monday } = this.props;
+        const mondayStartOfDay = moment(monday).startOf("day");
+        this.state.mondayTotal
+          ? this.submitBlock(this.state.mondayTotal, mondayStartOfDay, "monday")
+          : this.setState({ mondaySubmitComplete: true });
+        this.state.tuesdayTotal
+          ? this.submitBlock(
+              this.state.tuesdayTotal,
+              moment(mondayStartOfDay).add(1, "days"),
+              "tuesday"
+            )
+          : this.setState({ tuesdaySubmitComplete: true });
+        this.state.wednesdayTotal
+          ? this.submitBlock(
+              this.state.wednesdayTotal,
+              moment(mondayStartOfDay).add(2, "days"),
+              "wednesday"
+            )
+          : this.setState({ wednesdaySubmitComplete: true });
+        this.state.thursdayTotal
+          ? this.submitBlock(
+              this.state.thursdayTotal,
+              moment(mondayStartOfDay).add(3, "days"),
+              "thursday"
+            )
+          : this.setState({ thursdaySubmitComplete: true });
+        this.state.fridayTotal
+          ? this.submitBlock(
+              this.state.fridayTotal,
+              moment(mondayStartOfDay).add(4, "days"),
+              "friday"
+            )
+          : this.setState({ fridaySubmitComplete: true });
+        this.state.saturdayTotal
+          ? this.submitBlock(
+              this.state.saturdayTotal,
+              moment(mondayStartOfDay).add(5, "days"),
+              "saturday"
+            )
+          : this.setState({ saturdaySubmitComplete: true });
+        this.state.sundayTotal
+          ? this.submitBlock(
+              this.state.sundayTotal,
+              moment(mondayStartOfDay).add(6, "days"),
+              "sunday"
+            )
+          : this.setState({ sundaySubmitComplete: true });
+      }
+    );
   };
 
-  submitBlock = (hours, date) => {
+  submitBlock = (hours, date, day) => {
     const start_time = moment(date).add(9, "hours");
     const end_time = moment(start_time).add(hours, "hours");
     let block = {
@@ -102,8 +143,60 @@ class NewRowForm extends React.Component {
       status: "unSubmitted",
       manualEntry: true
     };
+    axios.post(`/api/timeblocks`, block).then(res => {
+      switch (day) {
+        case "monday":
+          this.setState({ mondaySubmitComplete: true }, () =>
+            this.checkSubmitComplete()
+          );
+          break;
+        case "tuesday":
+          this.setState({ tuesdaySubmitComplete: true }, () =>
+            this.checkSubmitComplete()
+          );
+          break;
+        case "wednesday":
+          this.setState({ wednesdaySubmitComplete: true }, () =>
+            this.checkSubmitComplete()
+          );
+          break;
+        case "thursday":
+          this.setState({ thursdaySubmitComplete: true }, () =>
+            this.checkSubmitComplete()
+          );
+          break;
+        case "friday":
+          this.setState({ fridaySubmitComplete: true }, () =>
+            this.checkSubmitComplete()
+          );
+          break;
+        case "saturday":
+          this.setState({ saturdaySubmitComplete: true }, () =>
+            this.checkSubmitComplete()
+          );
+          break;
+        case "sunday":
+          this.setState({ sundaySubmitComplete: true }, () =>
+            this.checkSubmitComplete()
+          );
+          break;
+      }
+    });
+  };
 
-    axios.post(`/api/timeblocks`, block).then(res => console.log(res));
+  checkSubmitComplete = () => {
+    if (
+      this.state.mondaySubmitComplete &&
+      this.state.tuesdaySubmitComplete &&
+      this.state.wednesdaySubmitComplete &&
+      this.state.thursdaySubmitComplete &&
+      this.state.fridaySubmitComplete &&
+      this.state.saturdaySubmitComplete &&
+      this.state.sundaySubmitComplete
+    ) {
+      this.props.getCurrentUserTimeBlocks();
+      this.handleCancel();
+    }
   };
 
   handleCancel = e => {
