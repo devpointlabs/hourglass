8544141a1d028c9570283534478d292ac64ad6a1
Chris Larsen <c.r.larsen@hotmail.com>
2019-04-25 14:49:58 -0600

    remove dropdown from regular user pages

--- client/src/components/timeblocks/TimeSheet.js
+++ client/src/components/timeblocks/TimeSheet.js
@@ -217,20 +217,21 @@ class TimeSheet extends React.Component {
           setView={this.setView}
           setSelectedWeek={this.setSelectedWeek}
         />
-        {this.state.view === "day" ? (
-          <Dropdown
-            onChange={this.filterUser}
-            placeholder="Teammates"
-            fluid
-            multiple
-            selection
-            options={options}
-            style={{ borderRadius: 0 }}
-            clearable
-            scrolling
-            value={this.state.filteredUserIds}
-          />
-        ) : (
+        {this.props.auth.user.admin &&
+          (this.state.view === "day" ? (
+            <Dropdown
+              onChange={this.filterUser}
+              placeholder="Teammates"
+              fluid
+              multiple
+              selection
+              options={options}
+              style={{ borderRadius: 0 }}
+              clearable
+              scrolling
+              value={this.state.filteredUserIds}
+            />
+          ) : (
             <Dropdown
               onChange={this.filterProject}
               placeholder="Projects"
@@ -243,7 +244,7 @@ class TimeSheet extends React.Component {
               scrolling
               value={this.state.filteredProjectIds}
             />
-          )}
+          ))}
         <div style={{ display: "flex", padding: "10px" }}>
           <AddTimeBlockButton
             projects={projects}
@@ -319,142 +320,3 @@ export class TripleConnectedTimeSheet extends React.Component {
 }
 
 export default withRouter(TripleConnectedTimeSheet);
-
-// get blocks
-// add hours to blocks
-// add user_name, project_name, task_name to blocks
-// setup blocks by week
-
-//blocksByWeek = [{week_start: "", week_end: "", date2019-04-01: [blocks with hours]}]
-
-///////////////////////previous version for reference
-/////////////////////////////////////////////////////////
-// componentDidMount() {
-//   this.getTimeBlocks();
-// }
-
-// getTimeBlocks = () => {
-//   //user_id!!! not always 8
-//   axios.get(`/api/timeblocks`).then(res =>
-//     this.setState({ timeBlocks: res.data }, () => {
-//       !this.checkForActiveTimeBlock() && this.addNewTimeBlock(false);
-//       this.updateDateRange(this.state.startDate, this.state.endDate);
-//     })
-//   );
-// };
-
-// updateDateRange = (startDate, endDate) => {
-//   this.setState({ startDate, endDate }, () => this.setWeeks());
-// };
-
-// setWeeks = () => {
-//   const { startDate, endDate, timeBlocks } = this.state;
-//   this.setState({
-//     weeks: groupTimeBlocksByWeek(timeBlocks, startDate, endDate)
-//   });
-// };
-
-// addTimeBlock = () => {
-//   this.getTimeBlocks();
-// };
-
-// updateBlocks = timeBlock => {
-//   this.setState(
-//     {
-//       timeBlocks: this.state.timeBlocks.map(t => {
-//         if (t.id === timeBlock.id) return { ...t, ...timeBlock };
-//         return t;
-//       })
-//     },
-//     () => {
-//       !this.checkForActiveTimeBlock() && this.addNewTimeBlock(false);
-//     }
-//   );
-// };
-
-// addNewTimeBlock = editMode => {
-//   this.setState(
-//     {
-//       timeBlocks: [...this.state.timeBlocks, { editMode: editMode }]
-//     },
-//     () => this.setWeeks()
-//   );
-// };
-
-// updateTimeBlock = timeblock => {
-//   this.setState(
-//     {
-//       timeblocks: this.state.timeBlocks.map(t => {
-//         if (t.id === timeblock.id) return timeblock;
-//         return t;
-//       })
-//     },
-//     () => this.setWeeks()
-//   );
-// };
-
-// checkForActiveTimeBlock = () => {
-//   let result = false;
-//   this.state.timeBlocks.map(t => {
-//     if (t.start_time && !t.end_time) result = true;
-//   });
-//   console.log(result);
-//   return result;
-// };
-
-// deleteTimeBlock = (id, project_id) => {
-//   axios.delete(`/api/timeblocks/${id}`).then(res => {
-//     this.setState({
-//       timeBlocks: this.state.timeBlocks.filter(t => t.id !== id)
-//     });
-//     this.setWeeks();
-//   });
-// };
-
-//   <div>
-//   <Table>
-//     <Table.Header>
-//       <Table.Row>
-//         <Table.HeaderCell style={{ paddingTop: "10px" }}>
-//           <Button
-//             style={{
-//               background: "#723186",
-//               color: "white",
-//               width: "25%"
-//             }}
-//             onClick={() => this.addNewTimeBlock(true)}
-//           >
-//             <Icon style={{ color: "white" }} name="add" />
-//           </Button>
-//         </Table.HeaderCell>
-//         <Table.HeaderCell style={{ width: "20%" }}>
-//           <DateRange
-//             startDate={this.state.startDate}
-//             endDate={this.state.endDate}
-//             updateDateRange={this.updateDateRange}
-//           />
-//         </Table.HeaderCell>
-//         <Table.HeaderCell>Date</Table.HeaderCell>
-//         <Table.HeaderCell>Start Time</Table.HeaderCell>
-//         <Table.HeaderCell>End Time</Table.HeaderCell>
-//         <Table.HeaderCell>Total Time</Table.HeaderCell>
-//         <Table.HeaderCell>Billable</Table.HeaderCell>
-//         <Table.HeaderCell>UnBillable</Table.HeaderCell>
-//         <Table.HeaderCell>Clock In/Out</Table.HeaderCell>
-//       </Table.Row>
-//     </Table.Header>
-//     <Table.Body>
-//       {this.state.weeks &&
-//         this.state.weeks.map(w => (
-//           <UserWeek
-//             key={w.title}
-//             week={w}
-//             updateBlocks={this.updateBlocks}
-//             addTimeBlock={this.addTimeBlock}
-//             deleteTimeBlock={this.deleteTimeBlock}
-//             updateTimeBlock={this.updateTimeBlock}
-//           />
-//         ))}
-//     </Table.Body>
-//   </Table>
-// </div>
